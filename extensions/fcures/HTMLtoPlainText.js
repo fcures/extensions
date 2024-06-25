81abe93985c14469e1f315991acb154962e205ae
sevan <140277466+fcures@users.noreply.github.com>
2024-06-25 13:13:30 -0700

    Create HTMLtoPlainText.js

---
+++ extensions/fcures/HTMLtoPlainText.js
@@ -0,0 +1,6 @@
+// Name: HTMLtoPlainText
+// ID: fcuresHTMLtoPlainText
+// Description: Allows you to use convert html text to plain text for ease of use.
+// By: fcures <https://scratch.mit.edu/users/fcures> (Main account is sev18 <https://scratch.mit.edu/users/sev18>)
+// License: MIT
+
