89b2f61a0a8a6d2e53e9081c9ed4a5b9ad6107cc
sevan <140277466+fcures@users.noreply.github.com>
2024-06-24 22:11:23 -0700

    Create ScratchPull.js

---
+++ extensions/fcures/ScratchPull.js
@@ -0,0 +1,6 @@
+// Name: ScratchPull
+// ID: fcuresScratchPull
+// Description: Allows you to pull info from scratch website, such as user info, project info, studio info, etc.
+// By: fcures (sev18) <https://scratch.mit.edu/users/sev18/>
+// License: MIT
+
